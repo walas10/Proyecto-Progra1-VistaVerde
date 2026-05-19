0e99b5185339c974f92b2e29beb3e54cd463ec5d
walas10 <156984089+walas10@users.noreply.github.com>
2026-05-18 22:05:20 -0600

    Repositorio con Neatbeans
    Se fusiona repositorio con proyecto neatbeans en carpeta.

---
+++ condominio_vistaverde/src/main/java/com/mycompany/condominio_vistaverde/Condominio_vistaverde.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.condominio_vistaverde;
+
+/**
+ *
+ * @author wesle
+ */
+public class Condominio_vistaverde {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
